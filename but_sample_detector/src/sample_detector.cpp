b964a29b8c2a90026d5f437738d6c052d851f97c
but-spanel <spanel@fit.vutbr.cz>
2012-06-25 14:37:59 +0200

    Legal notice modified

--- but_sample_detector/src/sample_detector.cpp
+++ but_sample_detector/src/sample_detector.cpp
@@ -1,13 +1,29 @@
-/**
- * Developed by dcgm-robotics@FIT group
+/******************************************************************************
+ * \file
+ *
+ * $Id:$
+ *
+ * Copyright (C) Brno University of Technology
+ *
+ * This file is part of software developed by dcgm-robotics@FIT group.
+ *
  * Author: Tomas Hodan (xhodan04@stud.fit.vutbr.cz)
- * Date: 01.04.2012 (version 1.0)
+ * Supervised by: Vitezslav Beran (beranv@fit.vutbr.cz), Michal Spanel (spanel@fit.vutbr.cz)
+ * Date: 01/04/2012
+ * Description: Sample detector demonstrating how to wrap a detector using ObjDet API into ROS.
  *
- * License: LGPL
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Description:
- * Sample detector demonstrating how to wrap a detector using ObjDet API into ROS.
- *------------------------------------------------------------------------------
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this file.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <ros/ros.h> // Main header of ROS
